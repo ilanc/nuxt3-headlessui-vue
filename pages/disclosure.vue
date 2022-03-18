58e745b8a838f37cd7ad2b0c61d3a0c9ff6ba382
ilan <ilan@spikedata.co.za>
2022-03-18 12:58:00 +0200

    Disclosure

--- pages/disclosure.vue
+++ pages/disclosure.vue
@@ -1,3 +1,44 @@
 <template>
-  <div>todo</div>
+  <div
+    class="w-screen h-screen flex flex-col items-center justify-center gap-4"
+  >
+    <div class="w-full px-4 pt-16">
+      <div class="w-full max-w-md p-2 mx-auto bg-white rounded-2xl">
+        <Disclosure v-slot="{ open }">
+          <DisclosureButton
+            class="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+          >
+            <span>What is your refund policy?</span>
+            <ChevronUpIcon
+              :class="open ? 'transform rotate-180' : ''"
+              class="w-5 h-5 text-purple-500"
+            />
+          </DisclosureButton>
+          <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
+            If you're unhappy with your purchase for any reason, email us within
+            90 days and we'll refund you in full, no questions asked.
+          </DisclosurePanel>
+        </Disclosure>
+        <Disclosure as="div" class="mt-2" v-slot="{ open }">
+          <DisclosureButton
+            class="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75"
+          >
+            <span>Do you offer technical support?</span>
+            <ChevronUpIcon
+              :class="open ? 'transform rotate-180' : ''"
+              class="w-5 h-5 text-purple-500"
+            />
+          </DisclosureButton>
+          <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
+            No.
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import { ChevronUpIcon } from "@heroicons/vue/solid";
+</script>

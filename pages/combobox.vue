a6f4c94c820ad2e31c52a2419c9003c7907071fc
ilan <ilan@spikedata.co.za>
2022-03-18 12:54:07 +0200

    combobox - fix overflow

--- pages/combobox.vue
+++ pages/combobox.vue
@@ -6,7 +6,7 @@
       <Combobox v-model="selected">
         <div class="relative mt-1">
           <div
-            class="relative w-full text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-teal-300 focus-visible:ring-offset-2 sm:text-sm overflow-hidden"
+            class="relative w-full text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-teal-300 focus-visible:ring-offset-2 sm:text-sm"
           >
             <ComboboxInput
               class="w-full border-none focus:ring-0 py-2 pl-3 pr-10 text-sm leading-5 text-gray-900"

d8109a0536edd5f18be04b7734f16322a6aac7e5
ilan <ilan@spikedata.co.za>
2022-03-18 12:56:25 +0200

    switch

--- pages/switch.vue
+++ pages/switch.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>todo</div>
+  <div
+    class="w-screen h-screen flex flex-col items-center justify-center gap-4"
+  >
+    <div class="py-16">
+      <Switch
+        v-model="enabled"
+        :class="enabled ? 'bg-teal-900' : 'bg-teal-700'"
+        class="relative inline-flex flex-shrink-0 h-[38px] w-[74px] border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        <span class="sr-only">Use setting</span>
+        <span
+          aria-hidden="true"
+          :class="enabled ? 'translate-x-9' : 'translate-x-0'"
+          class="pointer-events-none inline-block h-[34px] w-[34px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200"
+        />
+      </Switch>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { Switch } from "@headlessui/vue";
+
+const enabled = ref(false);
+</script>
